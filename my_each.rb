95a87fddb5da4e65f711c8d1812051cbeef6443a
Katerina  Papacostas <katpapacostas@gmail.com>
2017-10-22 23:32:11 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  return "This block should not run!" if array.length == 0
+
+  if block_given?
+    times_called = array.length - 1
+    i = 0
+
+    while i <= times_called
+      yield array[i]
+      i += 1
+    end
+
+  else
+    "Hey! No block was given!"
+  end
+  array
 end
